a44888d22ed3af059fedc614a401ded70a0bf2f1
Roshan Suganthan <38430708+suganros56@users.noreply.github.com>
2022-05-12 07:49:47 +0000

    connect metamask to DAO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+  
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to NarutoDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet conected to HaikyuuDAO</h1>
     </div>
   );
 };
